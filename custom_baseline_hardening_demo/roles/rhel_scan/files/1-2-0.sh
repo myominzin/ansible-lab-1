fd949d86bd852ce721b84f6c3aeb210c1672f37d
Myo Min Zin <komyominzin@gmail.com>
2020-10-21 11:46:18 +0630

    Update 1-2-0.sh

--- custom_baseline_hardening_demo/roles/rhel_scan/files/1-2-0.sh
+++ custom_baseline_hardening_demo/roles/rhel_scan/files/1-2-0.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-if [[ $(grep -c -e "^nameserver 192.168.1.1$" /etc/resolv.conf) -eq 1 && $(grep -c -e "^nameserver 192.168.1.2$" /etc/resolv.conf) -eq 1 && $(grep -c -e "^nameserver 10.20.10.30$" /etc/resolv.conf) -eq 1 && $(grep -c -e "^nameserver 10.20.10.31$" /etc/resolv.conf) -eq 1 && $(grep -c -e "^search opensource.com$" /etc/resolv.conf) -eq 1 ]]; then
+if [[ $(grep -c -e "^nameserver 192.168.123.1$" /etc/resolv.conf) -eq 1 && $(grep -c -e "^nameserver 192.168.100.1$" /etc/resolv.conf) -eq 1 && $(grep -c -e "^nameserver 192.167.100.113$" /etc/resolv.conf) -eq 1 && $(grep -c -e "^nameserver 192.168.100.1141$" /etc/resolv.conf) -eq 1 && $(grep -c -e "^search redhatmm.local$" /etc/resolv.conf) -eq 1 ]]; then
     exit 0
 else
     exit 1
